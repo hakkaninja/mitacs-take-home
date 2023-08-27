36debb7e4a846e01d961e5b154f7b5ea7b2e97b8
hakkaninja <mattwong789@gmail.com>
2023-08-27 13:08:44 -0700

    Update instructions and redis client

--- server/data-access/redisClient.js
+++ server/data-access/redisClient.js
@@ -1,7 +1,6 @@
 import { createClient } from "redis";
-const REDIS_PORT = process.env.REDIS_PORT || 5001;
 
-const redisClient = createClient(REDIS_PORT);
+const redisClient = createClient();
 redisClient.on("error", (err) => console.log("Redis Client Error", err));
 await redisClient.connect();
 
